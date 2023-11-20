9540c26e75d50d6a0ccfff5d9014e9e9af3757ac
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-11-20 22:16:36 +0100

    not cls

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None:
+        if not cls:
             return self.__objects
         else:
             filtered_dict = {}
