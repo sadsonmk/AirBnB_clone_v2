695bc345f70f3cdf24fed06fe289b5c6d9b39f09
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-11-20 22:03:17 +0100

    ok

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -53,7 +53,6 @@ class FileStorage:
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
-        return BaseModel, User, Place, State, City, Amenity, Review
 
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
