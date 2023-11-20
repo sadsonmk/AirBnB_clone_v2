cd8a5991e21997743b0966df887faae25cd02d3e
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-11-20 22:01:01 +0100

    ok

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,7 +71,7 @@ class FileStorage:
 
     def delete(self, obj=None):
         """Deletes 'obj' from '_objects'"""
-        if obj is not None:
+        if not obj:
             key = obj.__class__.__name__ + "." + obj.id
             if key in self.__objects:
                 del(self.__objects[key])
