63f02742bbb43afd89bad65402508eaddb683973
Sadson <sadsonmkumira@yahoo.com>
2023-12-10 16:34:57 +0000

    Adding fils

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,29 +7,21 @@ if ! command -v nginx &> /dev/null; then
 	sudo apt-get -y install nginx
 fi
 
-# folders and files to be created
-folders=(  "/data/web_static/releases/test/" "/data/web_static/shared/" )
-file="/data/web_static/releases/test/index.html"
-
 # check if folders already exist and create if non existent
-mkdir -p ${folders[0]}
-mkdir -p ${folders[1]}
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
 # check if files exist and create if they don't exist
-touch "$file"
-echo "<h1>Testing My website</h1>" >> "$file"
-
-# symbolic links
-link_to="/data/web_static/releases/test/"
-sym_link="/data/web_static/current"
+touch /data/web_static/releases/test/index.html
+echo "<h1>Testing My website</h1>" > /data/web_static/releases/test/index.html
 
 # recreates a symbolic link and -f forces overwriting if another link exists
-ln -sf "$link_to" "$sym_link"
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # change ownership and group ownership to ubuntu and give privileges
 sudo chown -R ubuntu:ubuntu /data/
 sudo chown -R 755 /data/
 # update the nginx configuration to serve content to hbnb_static
-sudo sed -i '48i \\t location /hbnb_static {\n\talias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
+sudo sed -i '48i \\t location /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
 # restart nginx
 sudo service nginx restart
